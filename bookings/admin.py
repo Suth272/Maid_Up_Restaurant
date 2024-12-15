d8caf0f162ae5d16951d56f1fb9ab1f7067fa0e0
Suth272 <suth272@outlook.com>
2024-12-15 08:10:44 +0000

    FEAT: Added admin model to the bookings app.

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Table, Reservation
 
-# Register your models here.
+admin.site.register(Table)  # Register Table model for admin
+admin.site.register(Reservation)  # Register Reservation model for admin
